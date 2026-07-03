64c48126a4f2445e009b69937e6548fa3dd915e3
RammiE007 <alexxendar1@gmail.com>
2026-07-03 14:56:31 +0530

    build: add release signing with keystore

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,3 +6,24 @@ plugins {
     alias(libs.plugins.secrets) apply false
     alias(libs.plugins.google.services) apply false
 }
+
+android {
+    signingConfigs {
+        create("release") {
+            val keystorePropertiesFile = rootProject.file("key.properties")
+            val keystoreProperties = java.util.Properties()
+            keystoreProperties.load(java.io.FileInputStream(keystorePropertiesFile))
+            
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+}
