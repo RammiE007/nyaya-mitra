ec869aa2d102d9dda0ee35f642463204d024ceb4
RammiE007 <alexxendar1@gmail.com>
2026-07-04 17:25:06 +0530

    Fix top-level build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,30 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+  GNU nano 9.1                  build.gradle.kts                  
+Modified
+// Top-level build file where you can add configuration options 
+// common to a>
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.google.devtools.ksp) apply false
+    alias(libs.plugins.roborazzi) apply false
     alias(libs.plugins.secrets) apply false
     alias(libs.plugins.google.services) apply false
 }
-
 android {
     signingConfigs {
         create("release") {
-            val keystorePropertiesFile = rootProject.file("key.properties")
+            val keystorePropertiesFile = 
+rootProject.file("key.properties")
             val keystoreProperties = java.util.Properties()
-            keystoreProperties.load(java.io.FileInputStream(keystorePropertiesFile))
-            
+            keystoreProperties.load(java.io.FileInputStream(keystorePropert>
+
             keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = file(keystoreProperties["storeFile"] as String)
-            storePassword = keystoreProperties["storePassword"] as String
+            keyPassword = keystoreProperties["keyPassword"] as 
+String
+            storeFile = file(keystoreProperties["storeFile"] as 
+String)
+            storePassword = keystoreProperties["storePassword"] as 
+String
         }
     }
     buildTypes {
@@ -26,3 +34,4 @@ android {
         }
     }
 }
+
