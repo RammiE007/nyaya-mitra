2c0bb445fa5da10290183c8ac4bac0b3e2143380
RammiE007 <alexxendar1@gmail.com>
2026-07-04 17:30:13 +0530

    Clean top-level build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,4 @@
-  GNU nano 9.1                  build.gradle.kts                  
-Modified
-// Top-level build file where you can add configuration options 
-// common to a>
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
@@ -10,28 +7,3 @@ plugins {
     alias(libs.plugins.secrets) apply false
     alias(libs.plugins.google.services) apply false
 }
-android {
-    signingConfigs {
-        create("release") {
-            val keystorePropertiesFile = 
-rootProject.file("key.properties")
-            val keystoreProperties = java.util.Properties()
-            keystoreProperties.load(java.io.FileInputStream(keystorePropert>
-
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as 
-String
-            storeFile = file(keystoreProperties["storeFile"] as 
-String)
-            storePassword = keystoreProperties["storePassword"] as 
-String
-        }
-    }
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            signingConfig = signingConfigs.getByName("release")
-        }
-    }
-}
-
